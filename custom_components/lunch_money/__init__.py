4af91614c79630e239e88c792f2989db0a749319
Noah Prail <nprail@event1.io>
2025-08-28 11:26:51 -0400

    fix

--- custom_components/lunch_money/__init__.py
+++ custom_components/lunch_money/__init__.py
@@ -8,7 +8,7 @@ async def async_setup_entry(hass, entry):
     """Set up Lunch Money from a config entry."""
     api_key = entry.data["api_key"]
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {"api": LunchMoneyAPI(api_key)}
-    await hass.config_entries.async_forward_entry_setups(entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass, entry):
